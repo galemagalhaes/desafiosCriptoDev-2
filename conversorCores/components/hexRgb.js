a6ab5590706da5abf1808174752829b6e4c8d5d5
galemagalhaes <g.alemagalhaes@gmail.com>
2022-05-21 15:34:44 -0300

    primeiro commit

---
+++ conversorCores/components/hexRgb.js
@@ -0,0 +1,25 @@
+var readline = require('readline');
+
+var leitor = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+leitor.question("Digite a cor em rgb\n", function(answer) {
+  var rgb = answer;
+  var array = rgb.split(',')
+  var hexSaida = rgbToHex(array[0], array[1], array[2])
+  console.log(hexSaida)
+  leitor.close();
+});
+
+function rgbToHex(r, g, b){
+  let x = parseInt(r)
+  let y = parseInt(g)
+  let z = parseInt(b)
+  var convert = [x, y, z].map(x => {
+    const hex = x.toString(16)
+    return hex.length === 1 ? '0' + hex : hex
+  }).join('')
+  return '#' + convert
+}
