6cb34b61a6f2ed4c57e14a898ef5d6b8db0655e2
FelipeGeazi <felipegeaziartes@gmail.com>
2022-05-21 17:15:20 -0300

    implementando servidor

--- conversorCores/components/rgbHex.js
+++ conversorCores/components/rgbHex.js
@@ -1,21 +1,25 @@
 var readline = require('readline');
 
 var leitor = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
 leitor.question("Digite a cor em hexadecimal\n", function(answer) {
-  var resp = answer;
-  console.log(hexRgb(resp))
-  leitor.close();
+    var resp = answer;
+    console.log(hexRgb(resp))
+    leitor.close();
 });
 
-function hexRgb(resp){
-  let hex_color = resp.replace("#", "")
-  let r = parseInt(hex_color.substring(0,2),16)
-  let g = parseInt(hex_color.substring(2,4),16)
-  let b = parseInt(hex_color.substring(4,6),16)
-  let rgbSaida = 'rgb('+ r +',' + g + ',' + b +')'
-  return rgbSaida
+function hexRgb(resp) {
+    let hex_color = resp.replace("#", "")
+    let r = parseInt(hex_color.substring(0, 2), 16)
+    let g = parseInt(hex_color.substring(2, 4), 16)
+    let b = parseInt(hex_color.substring(4, 6), 16)
+    let rgbSaida = 'rgb(' + r + ',' + g + ',' + b + ')'
+    return rgbSaida
+}
+
+module.exports = {
+    rgbToHex
 }
--- conversorCores/server/server.js
+++ conversorCores/server/server.js
@@ -1,11 +1,12 @@
 const http = require('http')
+const express = require('express')
 
 const port = 8080
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type" : "application/json"} )
-  response.end("Hello CryptoDev 2022!!")
+    response.writeHead(200, { "Content-Type": "application/json" })
+    response.end(rgbToHex, rgbToHex)
 })
 
 server.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`)
+    console.log(`Servidor rodando na porta ${port}`)
 })
